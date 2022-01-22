dc523f528aff9700f1ca63219e1a0165aaa68215
Volodymyr Tabashniuk <vtabashniuk@outlook.com>
2022-01-22 22:41:46 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class App extends Component {
+    render() {
+        return (
+            <>
+                <h1>Homework 2</h1>
+            </>);
+    }
+}
+
+export default App;
