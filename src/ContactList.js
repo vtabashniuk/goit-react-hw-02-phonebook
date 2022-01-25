cb2e4b5f355a6c5cb0ccac1090bdea10cb54fe78
Volodymyr Tabashniuk <vtabashniuk@outlook.com>
2022-01-25 08:20:52 +0200

    Добавил поле поиска контактов

--- src/ContactList.js
+++ src/ContactList.js
@@ -3,6 +3,8 @@ import React from "react";
 const ContactList = ({ contacts }) => (
   <div>
     <h2>Contacts</h2>
+    <label htmlFor="searchInput">Find contact by name</label>
+    <input id="searchInput" type="text" autoComplete="off"></input>
     <ul>
       {contacts.map(({ name, number }) => (
         <li key={name}>{name} : {number}</li>
